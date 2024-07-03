a692f1061f6f874b4032d8ec2986b0594a187963
aaron <aaronmussig@gmail.com>
2024-07-03 16:27:47 +1000

    remove gpiod

--- octoprint_octorelay/driver.py
+++ octoprint_octorelay/driver.py
@@ -30,14 +30,18 @@ class Relay():
     def is_closed(self) -> bool:
         """Returns the logical state of the relay."""
         # https://github.com/brgl/libgpiod/blob/master/bindings/python/examples/get_line_value.py
-        self.logger.info(f'checking closed {self.pin}')
+        self.logger.debug(f'>>> is_closed(pin={self.pin})')
         result = subprocess.check_output(["pinctrl", "get", str(self.pin)], encoding='utf-8')
+        self.logger.debug(f'>>>     result = {result}')
         hits = RE_OUTPUT.findall(result)
         if hits:
             pin_state = hits[0] == 'hi'
         else:
             pin_state = False
-        return xor(self.inverted, pin_state)
+        self.logger.debug(f'>>>     pin_state = {pin_state}')
+        out = xor(self.inverted, pin_state)
+        self.logger.debug(f'>>>     out = {out}')
+        return out
 
     def toggle(self, desired_state: Optional[bool] = None) -> bool:
         """
@@ -45,15 +49,20 @@ class Relay():
         If the argument is not specified then switches based on the current state.
         Returns the new logical state of the relay.
         """
+        self.logger.debug(f'>>> toggle(pin={self.pin}, {desired_state})')
         if desired_state is None:
             desired_state = not self.is_closed()
+            self.logger.debug(f'>>>     since desired_state is None, its now {desired_state}')
 
-        if xor(self.inverted, desired_state):
+        xor_value = xor(self.inverted, desired_state)
+        self.logger.debug(f'>>>     xor_value = {xor_value}')
+        if xor_value:
             value = "dh"
         else:
             value = "dl"
 
-        self.logger.info(f'>>> toggle {self.pin} {desired_state} = {value}')
-        result = subprocess.check_output(["pinctrl", "set", "11", "op", value, "pd"], encoding='utf-8')
-        self.logger.info(f'result: {result}')
+        self.logger.debug(f'>>>    pin {self.pin} is now getting set to {value}')
+        result = subprocess.check_output(["pinctrl", "set", str(self.pin), "op", value, "pd"], encoding='utf-8')
+        self.logger.debug(f'    result = {result}')
+        self.logger.debug(f'>>>     returning {desired_state}')
         return desired_state
